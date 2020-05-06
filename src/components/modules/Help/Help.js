4ec333049764de999a36eb1d954225b6e91e9c44
Glen Hodges <glenrhodges@gmail.com>
2020-05-06 10:11:03 -0400

    Changed layout of help component

--- src/components/modules/Help/Help.js
+++ src/components/modules/Help/Help.js
@@ -1,75 +1,80 @@
 import React from 'react'
+import './help.scss'
 import InnerContainer from '../../shared/InnerContainer/InnerContainer'
 import ArrowRight from '../../../svg/arrow-right-blue.svg'
 import '../../../styles/style.scss'
 
-export default () => (
-  <section id="help" className={`sectionPadding-sm ${style.helpWrapper}`}>
-    <InnerContainer>
-      <p className="pre-title text-center">Nurture Your Emotional Health</p>
-      <h3 className={`text-center ${style.title}`}>How can I help?</h3>
-      <div className={style.gridWrapper}>
-        <div className={style.gridItem}>
-          <h5 className={style.itemTitle}>Family Therapy</h5>
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            <li>Item 4</li>
-            <li>Item 5</li>
-          </ul>
-          <div className={style.btnWrapper}>
-            <a href="#">
-              Learn More <ArrowRight />
-            </a>
+const Help = () => {
+  return (
+    <section id="help" className="sectionPadding-sm helpWrapper">
+      <InnerContainer>
+        <p className="pre-title text-center">Nurture Your Emotional Health</p>
+        <h3 className="text-center title">How can I help?</h3>
+        <div className="gridWrapper">
+          <div className="gridItem">
+            <h5 className="itemTitle">Family Therapy</h5>
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+              <li>Item 4</li>
+              <li>Item 5</li>
+            </ul>
+            <div className="btnWrapper">
+              <a href="#">
+                Learn More <ArrowRight />
+              </a>
+            </div>
+          </div>
+          <div className="gridItem">
+            <h5 className="itemTitle">Couples Therapy</h5>
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+              <li>Item 4</li>
+              <li>Item 5</li>
+            </ul>
+            <div className="btnWrapper">
+              <a href="#">
+                Learn More <ArrowRight />
+              </a>
+            </div>
+          </div>
+          <div className="gridItem">
+            <h5 className="itemTitle">Family Therapy</h5>
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+              <li>Item 4</li>
+              <li>Item 5</li>
+            </ul>
+            <div className="btnWrapper">
+              <a href="#">
+                Learn More <ArrowRight />
+              </a>
+            </div>
+          </div>
+          <div className="gridItem">
+            <h5 className="itemTitle">Family Therapy</h5>
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+              <li>Item 4</li>
+              <li>Item 5</li>
+            </ul>
+            <div className="btnWrapper">
+              <a href="#">
+                Learn More <ArrowRight />
+              </a>
+            </div>
           </div>
         </div>
-        <div className={style.gridItem}>
-          <h5 className={style.itemTitle}>Couples Therapy</h5>
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            <li>Item 4</li>
-            <li>Item 5</li>
-          </ul>
-          <div className={style.btnWrapper}>
-            <a href="#">
-              Learn More <ArrowRight />
-            </a>
-          </div>
-        </div>
-        <div className={style.gridItem}>
-          <h5 className={style.itemTitle}>Family Therapy</h5>
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            <li>Item 4</li>
-            <li>Item 5</li>
-          </ul>
-          <div className={style.btnWrapper}>
-            <a href="#">
-              Learn More <ArrowRight />
-            </a>
-          </div>
-        </div>
-        <div className={style.gridItem}>
-          <h5 className={style.itemTitle}>Family Therapy</h5>
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            <li>Item 4</li>
-            <li>Item 5</li>
-          </ul>
-          <div className={style.btnWrapper}>
-            <a href="#">
-              Learn More <ArrowRight />
-            </a>
-          </div>
-        </div>
-      </div>
-    </InnerContainer>
-  </section>
-)
+      </InnerContainer>
+    </section>
+  )
+}
+
+export default Help
