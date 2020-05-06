cd1337182d25c8cc8901cec162e5d80d25baaacd
Glen Hodges <glenrhodges@gmail.com>
2020-05-06 08:07:00 -0400

    random fixes

--- src/components/modules/Cta/Cta.js
+++ src/components/modules/Cta/Cta.js
@@ -3,6 +3,7 @@ import style from './cta.module.scss'
 import Divider from '../../../svg/divider-line.svg'
 import InnerContainer from '../../shared/InnerContainer/InnerContainer'
 import Button from '../../shared/Button/Button'
+import { Link } from 'gatsby'
 
 export default () => (
   <section id="info-section" className={`sectionPadding ${style.infoSection}`}>
@@ -19,7 +20,7 @@ export default () => (
         </p>
         <Button
           btnText={'Learn More'}
-          link={'./about'}
+          link={'/about'}
           borderColor={'#0F4C81'}
           textColor={'#0F4C81'}
           backgroundColor={'#fff'}
