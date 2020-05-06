f44e3404cb120df9111595a068b1ec5d33edadab
Glen Hodges <glenrhodges@gmail.com>
2020-05-06 09:51:11 -0400

    trying to find why gatsby is erroring

--- src/components/modules/Help/Help.js
+++ src/components/modules/Help/Help.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import style from './help.module.scss'
 import InnerContainer from '../../shared/InnerContainer/InnerContainer'
-import ArrowRight from '../../../svg/arrow-right-blue.svg';
-import '../../../styles/style.scss';
+import ArrowRight from '../../../svg/arrow-right-blue.svg'
+import '../../../styles/style.scss'
 
 export default () => (
   <section id="help" className={`sectionPadding-sm ${style.helpWrapper}`}>
@@ -11,56 +11,64 @@ export default () => (
       <h3 className={`text-center ${style.title}`}>How can I help?</h3>
       <div className={style.gridWrapper}>
         <div className={style.gridItem}>
-            <h5 className={style.itemTitle}>Family Therapy</h5>
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                <li>Item 4</li>
-                <li>Item 5</li>
-            </ul>
-            <div className={style.btnWrapper}>
-                <a href="#">Learn More <ArrowRight /></a>
-            </div>
+          <h5 className={style.itemTitle}>Family Therapy</h5>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+          </ul>
+          <div className={style.btnWrapper}>
+            <a href="#">
+              Learn More <ArrowRight />
+            </a>
+          </div>
         </div>
         <div className={style.gridItem}>
-            <h5 className={style.itemTitle}>Couples Therapy</h5>
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                <li>Item 4</li>
-                <li>Item 5</li>
-            </ul>
-            <div className={style.btnWrapper}>
-                <a href="#">Learn More <ArrowRight /></a>
-            </div>
+          <h5 className={style.itemTitle}>Couples Therapy</h5>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+          </ul>
+          <div className={style.btnWrapper}>
+            <a href="#">
+              Learn More <ArrowRight />
+            </a>
+          </div>
         </div>
         <div className={style.gridItem}>
-            <h5 className={style.itemTitle}>Family Therapy</h5>
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                <li>Item 4</li>
-                <li>Item 5</li>
-            </ul>
-            <div className={style.btnWrapper}>
-                <a href="#">Learn More <ArrowRight /></a>
-            </div>
+          <h5 className={style.itemTitle}>Family Therapy</h5>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+          </ul>
+          <div className={style.btnWrapper}>
+            <a href="#">
+              Learn More <ArrowRight />
+            </a>
+          </div>
         </div>
         <div className={style.gridItem}>
-            <h5 className={style.itemTitle}>Family Therapy</h5>
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                <li>Item 4</li>
-                <li>Item 5</li>
-            </ul>
-            <div className={style.btnWrapper}>
-                <a href="#">Learn More <ArrowRight /></a>
-            </div>
+          <h5 className={style.itemTitle}>Family Therapy</h5>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+          </ul>
+          <div className={style.btnWrapper}>
+            <a href="#">
+              Learn More <ArrowRight />
+            </a>
+          </div>
         </div>
       </div>
     </InnerContainer>
