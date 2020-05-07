3007dda70d59082240ca4aad54cfe2bea9ef8903
Glen Hodges <glenrhodges@gmail.com>
2020-05-06 21:04:18 -0400

    fixed mobile menu issues

--- src/components/modules/Navigation/MobileMenu/MobileMenuItems/MobileMenuItems.js
+++ src/components/modules/Navigation/MobileMenu/MobileMenuItems/MobileMenuItems.js
@@ -3,24 +3,29 @@ import style from './mobileMenuItems.scss'
 import { Link } from 'gatsby'
 
 function MobileMenuItems() {
-  // const handleClick = () => {
-  //   var el = document.querySelector('.hamburger')
-  //   el.classList.toggle('is-active')
-  // }
+  const handleClick = () => {
+    let menuItem = document.querySelector(
+      '#mobile-menu-items .items-wrapper .item'
+    )
+    let mobileMenu = document.querySelector('#mobile-menu-items')
+    let body = document.querySelector('body')
+    body.classList.remove('fixed')
+    mobileMenu.classList.remove('is-shown')
+  }
 
   return (
     <div id="mobile-menu-items" className="mobileMenuItems">
       <div className="items-wrapper">
-        <Link className="item" to="/about">
+        <Link className="item" to="/about" onClick={handleClick}>
           about
         </Link>
-        <Link className="item" to="/services">
+        <Link className="item" to="/services" onClick={handleClick}>
           services
         </Link>
-        <Link className="item" to="/resources">
+        <Link className="item" to="/resources" onClick={handleClick}>
           resources
         </Link>
-        <Link className="item last" to="/contact">
+        <Link className="item last" to="/contact" onClick={handleClick}>
           contact
         </Link>
       </div>
