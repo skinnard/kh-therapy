2b25674d5bf4da0a7265f96fb90923c109db305b
Hodges Glen <glen.hodges-ext@kering.com>
2024-09-09 10:37:37 -0400

    Updated services page

--- src/components/modules/ServicesIntro/ServicesIntro.js
+++ src/components/modules/ServicesIntro/ServicesIntro.js
@@ -12,23 +12,12 @@ export default () => (
         <div className="contentWrapper">
           <div className="shape"></div>
           <div className="textWrapper">
-            <p className="subTitle">Why Therapy?</p>
+            <p className="subTitle">Psychotherapy Services</p>
             <p>
-              Choosing whether to pursue therapy can bring up many questions
-              within us. “Do I really need therapy?” “Are my problems and
-              experiences even worth talking about?” We can often feel insecure,
-              embarrassed, and even a little afraid to enter into therapy.
+              Depending on your needs, I provide psychotherapy services that
+              help you slow down on the stuck places in your life so you can
+              grow in life-changing ways.
             </p>
-
-            <p>
-              With this in mind, I not only validate these concerns, but I
-              encourage you all the more to reach out despite these feelings as
-              often time these emotions are indicative of how therapy can
-              actually be helpful for us. For some, it may be their first time
-              exploring therapy as an option.
-            </p>
-
-            <p>My hope is that you feel supported in our work together.</p>
           </div>
         </div>
       </div>
